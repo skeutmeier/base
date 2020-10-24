6b95127c9bc59c777f0662334c365fe71dc779ae
Sebastian <sebastian@keutmeier.de>
2020-10-24 11:13:31 +0200

    first commit

---
+++ Skeutmeier/Base/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © SKeutmeier BV All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Skeutmeier_Base', __DIR__);
+
