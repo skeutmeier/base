ac2509d5827c84427def8e11475dce92b0353e62
Sebastian <sebastian@keutmeier.de>
2020-10-24 11:30:22 +0200

    cleanup 2

--- Skeutmeier/Base/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © SKeutmeier BV All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Skeutmeier_Base', __DIR__);
-
